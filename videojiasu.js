25b04aadc4ddd17eb574e95a9f234815f5d4598b
Lcry <25002259+Lcry@users.noreply.github.com>
2018-09-29 16:46:07 +0800

    first commit
    first commit

---
+++ videojiasu.js
@@ -0,0 +1,6 @@
+/* 初始化 */
+document.querySelector('video').defaultPlaybackRate = 2.0;
+document.querySelector('video').play();
+
+/* 可自行设置快进倍数、当前设置10倍 */
+document.querySelector('video').playbackRate = 10.0;  
